2ab476fb9bb5f1139ca1cde480526bea05fc2df9
Antonio <antonio.dias8997@gmail.com>
2021-08-16 01:39:10 -0300

    Responsive changes, css refactor & start scripting

--- script.js
+++ script.js
@@ -1,65 +1,126 @@
-//fazer com que os valores sejam autenticados e adiconar borda para mostrar se está
-//certo ou errado, colocar pequeno texto 'can't be 0' no input se o valor colocado for 0
-//mandar o valor para as funções de cálculo e verificar qual botão está selecionado
-const inputValidation = {
-    billInputValidation() {
-        const bill = document.querySelector('.billInput')
-        const errorTitle = document.querySelector('#bill .titles h3')
+const bill = document.querySelector('#bill'),
+      people = document.querySelector('#people'),
+      billInput = document.querySelector('.billInput'),
+      peopleInput = document.querySelector('.peopleInput'),
+      selectedButtons = document.querySelector('#buttons .button.selected'),
+      customTip = document.querySelector('#customTip'),
+      buttons = document.querySelectorAll('#buttons .button')
+      
+let billValue = 0,
+    peopleValue = 0
+        
+    function calculateValues(bill, people, button, custom) {
+        if(bill.value > 0 && people.value > 0) {
+            if(custom.value > 0) {
+                const sum = Number(bill.value) + Number(people.value) + Number(custom.value)
+                const amount = document.querySelector('.amount h1 span')
+                amount.textContent = sum
+            } else {
+                const sum = Number(bill.value) + Number(people.value) + Number(button.value)
+                const amount = document.querySelector('.amount h1 span')
+                amount.textContent = sum
+            }
+        }
+    }
+    
+    customTip.addEventListener("click", (e) => {
+        buttons.forEach((btn) => {
+            if (btn.classList.contains('selected')) {
+                    btn.classList.remove('selected')
+            }
+        })
+    })
+       
+    
 
-        bill.addEventListener("change", (e) => {
-            if (bill.value <= '0' || bill.value == undefined || bill.value == '') {
-                bill.classList.add('error')
+const Validations = {
+    
+    InputValidation(element) {
+        const id = element.id
+        const input = document.querySelector(`#${id} input`)
+        console.log(id)
+        const errorTitle = document.querySelector(`#${id} .titles h3`)
+        element.addEventListener("change", (e) => {
+            if (input.value <= 0 || input.value == undefined || input.value == '') {
+                input.classList.add('error')
                 errorTitle.classList.add('active')
             } else {
-                bill.classList.remove('error')
+                let button
+                input.classList.remove('error')
                 errorTitle.classList.remove('active')
+                buttons.forEach((btn) => {
+                    if(btn.classList.contains('selected')) {
+                        button = btn
+                    }
+                })
+                calculateValues(billInput, peopleInput, button, customTip)
             }
         })
     },
 
-    peopleInputValidation() {
-        const people = document.querySelector('.peopleInput')
-        const errorTitle = document.querySelector('#peopleNumber .titles h3')
-        people.addEventListener("change", (e) => {
-            if (people.value <= '0' || people.value == undefined || people.value == '') {
-                people.classList.add('error')
-                errorTitle.classList.add('active')
-            } else {
-                people.classList.remove('error')
-                errorTitle.classList.remove('active')
-            }
-        })
-    }
-}
-
-const tipButtons = {
-    selectButtons() {
-        const buttons = document.querySelectorAll('#buttons .button')
+    selectButtonsValidation() {
         buttons.forEach((btns) => {
             btns.addEventListener("click", (e) => {
+                let button
                 buttons.forEach((btn) => {
                     if (btn.classList.contains('selected')) {
-                        btn.classList.remove('selected')
+                        btn.classList.toggle('selected')
                     }
                 })
-                e.target.classList.add('selected')
+                e.target.classList.toggle('selected')
+                customTip.value = 0
+                buttons.forEach((btn) => {
+                    if(btn.classList.contains('selected')) {
+                        button = btn
+                    }
+                })
+                calculateValues(billInput, peopleInput, button, customTip)
             })
         })
     },
 
     customTipValidation() {
-        const customTip = document.getElementById('customTip')
         customTip.addEventListener('change', (e) => {
-            if (customTip.value == '0' || customTip.value > 90 || customTip.value == '') {
+            if (customTip.value <= 0 || customTip.value > 99 || customTip.value == '') {
                 customTip.classList.add('error')
             } else {
+                let button
                 customTip.classList.remove('error')
+                buttons.forEach((btn) => {
+                    if(btn.classList.contains('selected')) {
+                        button = btn
+                    }
+                })
+                calculateValues(billInput, peopleInput, button, customTip)
             }
         })
     }
 }
 
-inputValidation.peopleInputValidation()
-inputValidation.billInputValidation()
-tipButtons.selectButtons()
-tipButtons.customTipValidation()
+const TipValues = {
+    
+
+    billValue() {
+        let bill = billValue
+        return bill
+    },
+
+    peopleValue() {
+        let people = peopleValue
+        return people
+    },
+
+    
+
+    /*
+        Verificar se todos os valores estão 'corretos' 
+        se todos estiverem corretos, realizar a conta
+    */
+    
+
+}
+
+Validations.InputValidation(bill)
+Validations.InputValidation(people)
+Validations.selectButtonsValidation()
+Validations.customTipValidation()
